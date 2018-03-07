d0b6a4c95bc76b9a4c60ee868deff3a07510bd3b
marioRamos55 <therasmus20092009@hotmail.es>
2018-03-07 13:44:04 -0600

    nuevo ejemplo

---
+++ HelloWord/src/HelloWord.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner Scan=new Scanner(System.in);
+		System.out.println("Ingresa tu nombre ");
+		String sNombre=Scan.nextLine();
+		System.out.println("tu nombre es "+sNombre);
+	}
+
+}
